94d4f6b1105319c81c2141c4d42b3ba3799d3dd3
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2016-02-18 16:33:38 -0500

    most recent stuff while pg broken for me

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,20 @@
 
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController< ApplicationController
+  def index
+    render({json: User.all})
+  end
+
+  def show
+  
+    render({json: User.find(params[:id])})
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: user
+  end
+  private
+  def user_params
+  params.require(:user).permit(:name, :email, :password_digest, :username, :age, :gender)
+  end
 end

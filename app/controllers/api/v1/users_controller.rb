74248958cd76596523f31e2ca0fe2364086618c6
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2016-02-18 17:02:31 -0500

    hi

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,11 +5,12 @@ class Api::V1::UsersController< ApplicationController
   end
 
   def show
-  
+
     render({json: User.find(params[:id])})
   end
 
   def create
+    binding.pry
     user = User.create(user_params)
     render json: user
   end
